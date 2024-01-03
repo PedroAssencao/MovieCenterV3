8e8bd225df7f8aea3b2a88278269af6ee6164854
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2024-01-03 13:48:15 -0300

    hg

--- scripts/Carrosel.js
+++ scripts/Carrosel.js
@@ -1,31 +1,67 @@
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
 
-$(document).ready(function(){
-    $('.responsive').slick({
-      slidesToShow: 7,
-      slidesToScroll: 1,
-    
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        }
-      ]
+    // Slide images according to the slide button clicks
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
     });
-});
+
+    // Show or hide slide buttons based on scroll position
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+    };
+
+    // Call handleSlideButtons when image list scrolls
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+    });
+
+    // Initialize slide buttons initially
+    handleSlideButtons();
+};
+
+//depois fazer esses carrosel com if por que isso aqui ta muito feio ta mlk
+
+const initSlider2 = () => {
+    const imageList = document.querySelector("#divdoslider #divdasimage");
+    const slideButtons = document.querySelectorAll("#divdoslider .buttonslegais");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    // Slide images according to the slide button clicks
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    // Show or hide slide buttons based on scroll position
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+    };
+
+    // Call handleSlideButtons when image list scrolls
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+    });
+
+    // Initialize slide buttons initially
+    handleSlideButtons();
+};
+
+
+window.addEventListener("resize", initSlider2);
+window.addEventListener("load", initSlider2);
+
+
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
